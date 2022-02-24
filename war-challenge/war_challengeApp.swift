ce9f88939b5f53423473542d6abac8c180bc7bd6
Burak <b.cuece@web.de>
2022-02-24 14:38:09 +0100

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Burak Cüce on 17.02.22.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
